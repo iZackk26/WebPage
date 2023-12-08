b56da71cd5a5ad6f1f94acd2383cb9861c6b7639
iZackk26 <newxstorecr@gmail.com>
2023-12-08 12:10:26 -0600

    feat: Adding Skills and images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,48 @@
 <template>
     <div class="home">
         <br>
-        <div class="row">
-            <div class="col-6" id="name">
-                <p><b>Hey, I'm Isaac</b></p>
-                I'm currently studying Computer Engineering at Tecnologico de Costa Rica. I like to create custom apps to learn new things.
-
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-6" id="name">
+                    <p><b>Hey, I'm Isaac</b></p>
+                    I'm currently studying Computer Engineering at Tecnologico de Costa Rica. I like to create custom apps
+                    to learn new things.
+                </div>
+                <div class="col-6" id="right-picture">
+                    <a href="https://github.com/iZackk26">
+                        <img class="img-fluid" src="../img/izack.jpg" id="picture" alt="Isaac's Profile Image">
+                    </a>
+                </div>
             </div>
-            <div class="col-6" id="right-picture">
-                <img src="../img/izack.jpg" id="picture" alt="Isaac's Profile Image">
+            <h1 class="mb-3 mt-3" id="skills">Skills</h1>
+            <div class="row rowcols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="../img/Futuristic-PC-Parts.png" alt="Skill-1">
+                        <div class="card-body">
+                            <h5 class="card-title">Hardware</h5>
+                            <p class="card-text">Text!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="../img/Software.png" alt="Skill-2">
+                        <div class="card-body">
+                            <h5 class="card-title">Software</h5>
+                            <p class="card-text">Text!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="../img/Cloud-Computing.png" alt="" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Cloud Computing</h5>
+                            <p class="card-text">Text!</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -39,10 +73,14 @@ body {
 }
 
 #picture {
-    width: 200px;
-    height: 200px;
+    width: 250px;
+    height: 250px;
     border-radius: 50%;
     object-fit: cover;
+}
+
+#skills {
+    text-align: left;
 }
 </style>
 
