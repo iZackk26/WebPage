e004fde3a818f0830ecf377aa4cb48843054c1e0
iZackk26 <newxstorecr@gmail.com>
2023-12-08 11:15:55 -0600

    feat: Adding Pre-Description and Image in a container

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,48 @@
 <template>
-  <div class="home">
-      <h1>Hola</h1>
-  </div>
+    <div class="home">
+        <br>
+        <div class="row">
+            <div class="col-6" id="name">
+                <p><b>Hey, I'm Isaac</b></p>
+                I'm currently studying Computer Engineering at Tecnologico de Costa Rica. I like to create custom apps to learn new things.
+
+            </div>
+            <div class="col-6" id="right-picture">
+                <img src="../img/izack.jpg" id="picture" alt="Isaac's Profile Image">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-  },
+    name: 'HomeView',
+    components: {
+    },
 });
 </script>
+
+<style>
+body {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+#name {
+    font-size: 24px;
+    padding-left: 35px;
+    text-align: left;
+
+}
+
+#picture {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+</style>
+
