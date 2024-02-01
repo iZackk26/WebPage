8c2047548870875454cee7a6d2685aba0efc8099
izackk26 <newxstorecr@gmail.com>
2024-01-31 22:04:56 -0600

    Feat: Adding the @izack and @ram on the Rap footer

--- src/views/Rap.vue
+++ src/views/Rap.vue
@@ -14,6 +14,25 @@
           </iframe>
       </div>
   </div>
+  <div class="container">
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+              <li class="nav-item">
+                  <a href="https://www.instagram.com/izackk_r/" target="_blank" class="nav-link px-2 text-body-secondary">
+                  <img src="../img/instagram-color.png" alt="" id="footer-img-right">
+                  iZack
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a href="https://www.instagram.com/ortizbarquero/" target="_blank" class="nav-link px-2 text-body-secondary">
+                  <img src="../img/instagram-color.png" alt="" id="footer-img-right">
+                  Ram
+                  </a>
+              </li>
+        </ul>
+        <p class="text-center text-body-secondary">© 2024 iZack</p>
+      </footer>
+  </div>
 </template>
 
 <style>
