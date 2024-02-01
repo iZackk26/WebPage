274b515ed71a9dbbf4892c5185185469bff8afaa
izackk26 <newxstorecr@gmail.com>
2024-01-31 22:04:38 -0600

    Feat: Adding the @izack footer on the Reggaeton and the 90's playlists

--- src/views/Nineties.vue
+++ src/views/Nineties.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="container">
-      <p class="text-md">90'S</p>
+      <h1 class="mb-3 mt-3" id="text"><b>- 90's</b></h1>
+      <p id="text">Step into the '90s groove with hits like 'Let's Groove,' 'Bohemian Rhapsody,' and 'Sweet Child O' Mine.' This playlist is a journey through iconic tracks from Earth, Wind & Fire, Queen, Michael Jackson, and more. Relive the magic and energy of the '90s with timeless classics that shaped a generation!</p>
+      <div class="spotify-player">
+          <iframe 
+              style="border-radius:12px"
+              src="https://open.spotify.com/embed/playlist/7x54BFwbbXcXYfHXYMo12l?utm_source=generator"
+              width="100%"
+              height="650"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy">
+          </iframe>
+      </div>
   </div>
+  <div class="container">
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+              <li class="nav-item">
+                  <a href="https://www.instagram.com/izackk_r/" target="_blank" class="nav-link px-2 text-body-secondary">
+                  <img src="../img/instagram-color.png" alt="" id="footer-img-right">
+                  iZack
+                  </a>
+              </li>
+        </ul>
+        <p class="text-center text-body-secondary">© 2024 iZack</p>
+      </footer>
+  </div>
+
 </template>
--- src/views/Reggaeton.vue
+++ src/views/Reggaeton.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="container">
-      <p class="text-md">Reggaeton</p>
+      <h1 class="mb-3 mt-3" id="text"><b>- That's why she left me</b></h1>
+      <p id="text">Dive into the heart of Latin urban beats with this playlist showcasing the best of reggaeton from influential and current artists. Experience the vibrant rhythms that define today's Latin music scene. Immerse yourself in the sounds of chart-toppers, creating a dynamic and engaging musical journey!</p>
+      <div class="spotify-player">
+          <iframe style="border-radius:12px"
+              src="https://open.spotify.com/embed/playlist/7o4mLd5W8KZEjgFF1JUFPo?utm_source=generator"
+              width="100%"
+              height="650"
+              frameBorder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy">
+          </iframe>
+      </div>
+      <div class="container">
+          <footer class="py-3 my-4">
+              <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                  <li class="nav-item">
+                      <a href="https://www.instagram.com/izackk_r/" target="_blank" class="nav-link px-2 text-body-secondary">
+                          <img src="../img/instagram-color.png" alt="" id="footer-img-right">
+                          iZack
+                      </a>
+                  </li>
+              </ul>
+              <p class="text-center text-body-secondary">© 2024 iZack</p>
+          </footer>
+      </div>
   </div>
 </template>
